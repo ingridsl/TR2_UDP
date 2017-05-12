b03c312feb39c781a00a845c4331ccbcba4c77a9
ingridsl <inus2agome@gmail.com>
2017-05-12 07:36:24 -0300

    for não funciona.
    tentando fazer a checagem através da soma dos bits

--- UDPCli.py
+++ UDPCli.py
@@ -1,4 +1,11 @@
 from socket import *
+from decimal import *
+import pickle
+import hashlib
+import sys
+import os
+import math
+import time
 serverName = '' 	#IP do servidor?
 serverPort = 12000 	# porta do servidor
 
@@ -7,9 +14,33 @@ clientSocket = socket(AF_INET, SOCK_DGRAM)
 
 #funcionamento?
 message = raw_input('Input lowercase sentence:') 
+newmes = ' '.join(format(ord(x), 'b') for x in message)
+print newmes
 
+val=0
+print newmes[3]
+
+tam = len(newmes)
+arre = range(0, tam)
+print tam
+arre2 = list(range(0, tam))
+
+for i in range(0, tam)
+	#if(newmes[i]=='1' ||newmes[i] =='0)
+	#	val+= newmes[x]
+
+print val
 #envia/recebe dados
-clientSocket.sendto(message,(serverName, serverPort)) 
+checksum = hashlib.md5()
+md5 = checksum
+print len(str(checksum))
+checksum.update (message)
+checksum.digest()
+print len(str(checksum))
+print str(checksum)
+
+clientSocket.sendto(str(str(checksum)+message),(serverName, serverPort)) 
+#clientSocket.sendto(md5,(serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048) 
 print modifiedMessage
 
--- UDPSrv.py
+++ UDPSrv.py
@@ -20,24 +20,31 @@ ack = []
 print 'The server is ready to receive'
 #envia/recebe dados
 while 1:
-	rcvpkt=[]
-	message, clientAddress = serverSocket.recvfrom(2048)
-	rcvpkt = pickle.loads(message) #dando erro aqui
-	print rcvpkt
-	print message
+	received_hash=[]
+	received_message = []
+	full_info, clientAddress, = serverSocket.recvfrom(2048)
+	
+	print full_info
+	received_hash = full_info[0:34]
+	message = full_info[34:]
+	message+"\0"
+
+	print "\nreceived_hash: " + received_hash
+	print "\nreceived_message: " + message
+
 #		check value of checksum received (c) against checksum calculated (h) - NOT CORRUPT
-	c = rcvpkt[-1]
-	del rcvpkt[-1]
-	h = hashlib.md5()
-	print h
-	print c
-	h.update(pickle.dumps(message))
-	print h.digest()
-	print 'the end'
-	if message == h.digest():
-		print 'oi'
+	#c = rcvpkt[-1]
+	#del rcvpkt[-1]
+	calculated_hash = hashlib.md5(message)
+	#print c
+	calculated_hash.update(message)
+	calculated_hash.digest()
+	print "\ncalculated_hash.digest(): " + str(calculated_hash)
+	print '\nthe end: '
+	if received_hash == calculated_hash:
+		print '\nAEEEEEEEE'
 	else:
-		print "error detected"
+		print "\nerror detected"
 	#print checksum
 	#if(checksum == 1 and message[48:64] == '01' * 8):
 	#	print "New Ack"
