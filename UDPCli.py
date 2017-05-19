3885c36b7720b45c15827d5869fa89af88909fea
ingridsl <inus2agome@gmail.com>
2017-05-19 20:18:58 -0300

    versão thread

--- UDPCli.py
+++ UDPCli.py
@@ -17,62 +17,69 @@ clientSocket.settimeout(0.001)
 base = 0
 seq = 0
 windowSize = 7
-print "tamanho da janela = 7"
 window = []
 fim = False
 f = open ("arq_teste.txt")
 lastackrcv = time.time()
+lastpkgsnd = 0 ###
 
 message = f.readline()
 while not fim or window:
-	if (seq < base + windowSize) and not fim:
-		print message
+	if (time.time()-lastpkgsnd > 0.001): ###
+		if (seq < base + windowSize) and not fim:
+			print "\nSEND" ###
+			print message
 
-		auxMsg = message
-		sum = 0
-		if (len(auxMsg) % 2) != 0:
-			auxMsg += "0"
-		for i in range(0, len(auxMsg), 2):
-			msg16 = ord(auxMsg[i]) + (ord(auxMsg[i+1]) << 8)
-			sum += msg16
-			sum = (sum & 0xffff) + (sum >> 16)
-		cs = ~sum & 0xffff
-		print "Checksum: " + str(cs)
+			auxMsg = message
+			sum = 0
+			if (len(auxMsg) % 2) != 0:
+				auxMsg += "0"
+			for i in range(0, len(auxMsg), 2):
+				msg16 = ord(auxMsg[i]) + (ord(auxMsg[i+1]) << 8)
+				sum += msg16
+				sum = (sum & 0xffff) + (sum >> 16)
+			cs = ~sum & 0xffff
+			print "Checksum: " + str(cs)
 
-		print "Sequence number: " + str(seq)
+			print "Sequence number: " + str(seq)
 
-		sndpkg = message + "*" + str(cs) + "*" + str(seq)
-		window.append(sndpkg)
-
-		r = random.random()
-		if (message != '') and (r <= 0.1): #simulacao de dado corrompido, probabilidade de 0.1
-			print '\033[91m' + '\nSimulating bit error' + '\033[0m'
-			error = random.getrandbits(8)
-			listMsg = list(message)
-			for i in range(5): #introduz 5 bits errados a mensagem
-				randByte = random.randint(0, len(listMsg)-1)
-				altByte = ord(listMsg[randByte]) & error
-				listMsg[randByte] = struct.pack("B", altByte)
-			message = "".join(listMsg)
 			sndpkg = message + "*" + str(cs) + "*" + str(seq)
-		clientSocket.sendto(sndpkg,(serverName, serverPort)) 
+			window.append(sndpkg)
 
-		seq += 1
-		if message == '':
-			fim = True 
-		message = f.readline()
-	else:
-		if not(seq < base + windowSize):
-			print "buffer cheio..."
+			r = random.random()
+			if (message != '') and (r <= 0.1): #simulacao de dado corrompido, probabilidade de 0.1
+				print '\033[91m' + '\nSimulating bit error' + '\033[0m'
+				error = random.getrandbits(8)
+				listMsg = list(message)
+				for i in range(5): #introduz 5 bits errados a mensagem
+					randByte = random.randint(0, len(listMsg)-1)
+					altByte = ord(listMsg[randByte]) & error
+					listMsg[randByte] = struct.pack("B", altByte)
+				message = "".join(listMsg)
+				sndpkg = message + "*" + str(cs) + "*" + str(seq)
+			clientSocket.sendto(sndpkg,(serverName, serverPort)) 
+			lastpkgsnd = time.time() ###
+
+			seq += 1
+			if message == '':
+				fim = True 
+			message = f.readline()
+		else:
+			if not(seq < base + windowSize):
+				print "buffer cheio..."
 
 	try:
 		ackMessage, serverAddress = clientSocket.recvfrom(2048)
+		#r = random.random()
+		#if not fim and (r <= 0.05): #simulacao de perda de pacote, probabilidade de 0.05
+		#	print  '\033[91m'+'\nSimulating acknowledgement loss\n'+'\033[0m'
+		#	continue
 
 		modifiedMessage, middle, received_cs = ackMessage.partition("*")
 		received_cs, middle, exp_seq = received_cs.partition("*")
 		print '\033[93m' + modifiedMessage + '\033[0m'
 
-		while int(exp_seq) > int(base) and window: #andando com janela
+		while int(exp_seq) > int(base) and window:
 			lastackrcv = time.time()
 			del window[0]
 			base += 1
@@ -82,8 +89,9 @@ while not fim or window:
 			break;
 
 	except:
-		if (time.time()-lastackrcv > 0.01):
-			print '\nERRO: ' + str(base)
+		print "\nEXCEPT: " + str(time.time()) + " - " + str(lastackrcv) + " = " + str(time.time()-lastackrcv) ###
+		if (time.time()-lastackrcv > 0.002): #0.01 ###
+			print '\nERRO: ' + str(base) + "->" + str(seq) ###
 			for i in window:
 				clientSocket.sendto(i,(serverName, serverPort)) 
 
